239690de10b8b572b70df05292eb398ab06bb09e
emmanuelTwumasi <protwumasi@gmail.com>
2025-02-12 23:53:55 +0000

    feat: add ci pipeline

--- api/routes/books.py
+++ api/routes/books.py
@@ -54,7 +54,7 @@ async def get_book_by_id(book_id:int) -> Book:
     if book is None:
         return JSONResponse(
             status_code = status.HTTP_404_NOT_FOUND,
-            content = {"message":"Book not found"}
+            content = {"detail":"Book not found"}
         )
     return book
 
