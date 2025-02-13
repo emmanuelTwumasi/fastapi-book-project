dd3594d58a6dbeff450595b9bce45ee99173584f
emmanuelTwumasi <58420781+emmanuelTwumasi@users.noreply.github.com>
2025-02-13 18:17:23 +0000

    fix: update cd with correct path

--- api/routes/books.py
+++ api/routes/books.py
@@ -46,6 +46,7 @@ async def create_book(book: Book):
 )
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+    
 
 @router.get("/{book_id}", response_model= Book, status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id:int) -> Book:
